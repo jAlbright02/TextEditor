598c0431b8204624641fc1168c8b1a6dcf0d2c7e
James Albright <G00379271@atu.ie>
2023-07-29 16:56:27 +0100

    Edited text font/size and more settings.

--- src/main/java/Editor.java
+++ src/main/java/Editor.java
@@ -5,11 +5,22 @@ import java.awt.event.ActionListener;
 
 public class Editor extends JFrame implements ActionListener {
 
+    JTextArea textArea;
+
+    //Constructor for GUI
     Editor() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Text Editor");
         this.setSize(500, 500);
         this.setLayout(new FlowLayout());this.setLocationRelativeTo(null);
+
+        textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(450, 450));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("SansSerif", Font.PLAIN, 22));
+
+        this.add(textArea); //adding to scroll pane later
         this.setVisible(true);
     }
 
