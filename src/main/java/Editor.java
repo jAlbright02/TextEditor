2d0118f88a446f9848945dbfcc60aa56f69f0712
James Albright <G00379271@atu.ie>
2023-07-29 16:45:17 +0100

    Basics of editor frame done

--- src/main/java/Editor.java
+++ src/main/java/Editor.java
@@ -1,2 +1,20 @@
-public class Editor {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Editor extends JFrame implements ActionListener {
+
+    Editor() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Text Editor");
+        this.setSize(500, 500);
+        this.setLayout(new FlowLayout());this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
