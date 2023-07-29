942f7002d57db14c370fcd5d35228da7d0856d5e
James Albright <G00379271@atu.ie>
2023-07-29 17:37:47 +0100

    Added font size box, font family spinner and a menu for opening, saving and closing the application.
    Exit and save options are working.

--- src/main/java/Editor.java
+++ src/main/java/Editor.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Editor extends JFrame implements ActionListener {
 
     JTextArea textArea;
     JScrollPane scrollPane;
+    JSpinner fontSizeSpinner;
+    JLabel fontLabel;
+    JComboBox fontBox;
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
 
     //Constructor for GUI
     Editor() {
@@ -24,12 +37,86 @@ public class Editor extends JFrame implements ActionListener {
         scrollPane.setPreferredSize(new Dimension(450, 450));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 
+        fontLabel = new JLabel("Font size: ");
+
+        fontSizeSpinner = new JSpinner();
+        fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
+        fontSizeSpinner.setValue(22);
+        fontSizeSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                textArea.setFont(new Font(textArea.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
+
+            }
+        });
+
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        fontBox = new JComboBox(fonts);
+        fontBox.addActionListener(this);
+        fontBox.setSelectedItem("SansSerif");
+
+        //Menu Bar -------------------
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+
+        //------------------------ Menu
+
+        this.setJMenuBar(menuBar);
+        this.add(fontBox);
+        this.add(fontLabel);
+        this.add(fontSizeSpinner);
         this.add(scrollPane);
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == fontBox) {
+            textArea.setFont(new Font((String) fontBox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
+        }
+
+        if (e.getSource() == openItem) {
+
+        }
+
+        if (e.getSource() == saveItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("C:\\Users\\user\\Documents\\TextEditor"));
+
+            int usrResponse = fileChooser.showSaveDialog(null);
+            if (usrResponse == JFileChooser.APPROVE_OPTION) {
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } finally {
+                    fileOut.close();
+                }
+            }
+        }
+
+        if (e.getSource() == exitItem) {
+            System.exit(0);
+        }
 
     }
 }
